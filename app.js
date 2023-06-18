139fcbee44cba57e4ae510359b7c79219905d36d
Arslan <arslan99111@gmail.com>
2023-06-18 17:41:27 +0500

    Started Working on JS

---
+++ app.js
@@ -0,0 +1,5 @@
+let playerText = document.getElementById("playerText");
+let restartBtn = document.getElementById("restartBtn");
+let boxes = Array.from(document.getElementsByClassName("box"));
+
+console.log(boxes);
