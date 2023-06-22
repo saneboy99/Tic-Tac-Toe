0af6840bbb9127f4f2bd3fa1704082fc8e1013a8
Arslan <arslan99111@gmail.com>
2023-06-22 12:15:14 +0500

    More Fuctionality

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ let playerText = document.getElementById("playerText");
 let restartBtn = document.getElementById("restartBtn");
 let boxes = Array.from(document.getElementsByClassName("box"));
 
+let winnerIndicator = getComputedStyle(document.body).getPropertyValue(
+  "--winning_blocks"
+);
+
 const O_TEXT = "O";
 const X_TEXT = "X";
 let currentPlayer = X_TEXT;
@@ -19,8 +23,39 @@ function boxClicked(e) {
     spaces[id] = currentPlayer;
     e.target.innerText = currentPlayer;
 
+    if (playerHasWon() !== false) {
+      playerText = `${currentPlayer} has won!`;
+      let winning_blocks = playerHasWon();
+
+      winning_blocks.map(
+        (box) => (boxes[box].style.backgroundColor = winnerIndicator)
+      );
+    }
+
     currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT;
   }
+}
+
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+function playerHasWon() {
+  for (const condtion of winningCombos) {
+    let [a, b, c] = condtion;
+
+    if (spaces[a] && spaces[a] == spaces[b] && spaces[a] == spaces[c]) {
+      return [a, b, c];
+    }
+  }
+  return false;
 }
 
 restartBtn.addEventListener("click", restart);
@@ -31,6 +66,8 @@ function restart() {
   boxes.forEach((box) => {
     box.innerText = "";
   });
+
+  playerText = "Tic Tac Toe";
 
   currentPlayer = X_TEXT;
 }
